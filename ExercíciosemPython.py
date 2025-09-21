f8e9ed49e185471e64a9e5d7147a4806bfe51e0c
Xavier-sa <wellington222688@gmail.com>
2025-09-21 18:40:06 -0300

    exercicio 1 da lista concluido

---
+++ ExercíciosemPython.py
@@ -0,0 +1,6 @@
+#exercicio feito 
+#01 - concluido
+
+numeroinseridoUsuario= int(input("Informe o N°:\n"))
+antecessor = numeroinseridoUsuario - 1
+print(f"O numero do usuario é{numeroinseridoUsuario}\n Seu antecessor é :{antecessor}")
