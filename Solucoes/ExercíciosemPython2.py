2b8d1a81e5a7f8d01a72916b4c93edf53f6b5ce7
W.Xavier <wellington222688@gmail.com>
2025-09-22 17:40:51 -0400

    Update ExercíciosemPython2.py
    Responde ao exercício 2- área do retângulo

--- Solucoes/ExercíciosemPython2.py
+++ Solucoes/ExercíciosemPython2.py
@@ -1,1 +1,5 @@
-#criando arquivo 
+
+baseRetangulo=float(input("Informe a Base:\n"))
+alturaRetangulo=float(input("Informe a Altura:\n"))
+areaRetangulo=baseRetangulo*alturaRetangulo
+print(f"Area do RETANGULO: {areaRetangulo}")
