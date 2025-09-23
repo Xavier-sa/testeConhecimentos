b9ee915e62f09473349b2b4e7c9fc036d896c7be
Xavier-sa <wellington222688@gmail.com>
2025-09-23 15:35:53 -0300

    praticando incompleto

---
+++ Solucoes/EntradaSaidaDados/1.py
@@ -0,0 +1,18 @@
+idadeAtleta = int(input("Qual a idade do atleta?:"))
+print(f"{'-'*10} Categorias:{'-'*10}")
+infantil="5 - 7 anos"
+iniciado="8 - 10 anos"
+juvenil= "11 - 13 anos"
+junior= "14 - 17 anos"
+senior=" MAIORES DE 18 anos"
+print(f"Infantil = {infantil}")
+print(f"inciado = {iniciado}")
+print(f"Juvenil = {juvenil}")
+print(f"Junior = {junior}")
+print(f"Senior = {senior}")
+#em desenvolimetno
+if idadeAtleta
+    print(infantil)
+
+else:
+    print("Idade Invalida")
