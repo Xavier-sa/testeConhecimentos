cbd2e75ac7dac585f334de739d33d773605dfa54
W.Xavier <wellington222688@gmail.com>
2025-09-24 13:06:02 -0400

    Update 1.py
    Atualizando e corrigindo

--- Solucoes/EntradaSaidaDados/1.py
+++ Solucoes/EntradaSaidaDados/1.py
@@ -1,4 +1,4 @@
-idadeAtleta = int(input("Qual a idade do atleta?:"))
+
 print(f"{'-'*10} Categorias:{'-'*10}")
 infantil="5 - 7 anos"
 iniciado="8 - 10 anos"
@@ -11,8 +11,16 @@ print(f"Juvenil = {juvenil}")
 print(f"Junior = {junior}")
 print(f"Senior = {senior}")
 #em desenvolimetno
-if idadeAtleta
+idadeAtleta = int(input("Qual a idade do atleta?:"))
+if idadeAtleta >=18:
+    print(senior)
+elif 5 <= idadeAtleta <=7:
     print(infantil)
-
+elif 8 <= idadeAtleta <=10:
+    print(iniciado)
+elif 11<= idadeAtleta <=13:
+    print(juvenil)
+elif 14 <= idadeAtleta <=17:
+    print(junior)
 else:
     print("Idade Invalida")
