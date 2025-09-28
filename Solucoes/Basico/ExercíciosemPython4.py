65505ff9ef641c70fdbcf6db22b6dfd7baac77d7
Xavier-sa <wellington222688@gmail.com>
2025-09-28 18:20:29 -0300

    feat:Converte idade para Dias e vice Versa.. incompleto

---
+++ Solucoes/Basico/ExercíciosemPython4.py
@@ -0,0 +1,16 @@
+ano = 12
+mes = 30
+dia = 1
+
+# um_ano_tem = 365 
+idade= int(input("Qual sua idade?\n"))
+
+
+idade_em_dias = idade*ano *mes * dia
+print(f"Sua idade em Dias é : {idade_em_dias}")
+
+dias_para_ano = idade_em_dias/ano/mes/dia
+print(f"Sua idade em Ano é igual :{dias_para_ano}")
+
+
+
