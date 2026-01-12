dc1dbfb5019a96b63d8e736de4af370afe84bea1
xavier-sa <wellington222688@gmail.com>
2026-01-12 10:24:47 -0300

    resolucao ex 05

---
+++ solucoes/xavier/ex05.py
@@ -0,0 +1,15 @@
+"""
+Exercício 05 — Entrada do usuário
+
+Peça ao usuário que digite seu nome.
+Depois, imprima:
+
+"Olá, <nome>! Seja bem-vindo(a)."
+"""
+
+# Escreva seu código abaixo
+
+
+username = input("Informe seu nome:\n ").upper()
+
+print(f"Olá,{username}! Seja bem-vindo(a).")
