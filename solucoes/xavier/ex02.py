00b7979bf792acf15a7aaaac3ee2060a44764d47
xavier-sa <wellington222688@gmail.com>
2026-01-11 23:08:02 -0300

    iniciando a lista

---
+++ solucoes/xavier/ex02.py
@@ -0,0 +1,21 @@
+"""
+Exercício 02 — Operações matemáticas
+
+Crie duas variáveis com números inteiros.
+Calcule e imprima:
+
+- Soma
+- Subtração
+- Multiplicação
+- Divisão
+"""
+
+
+numero1 = 10
+numero2 = 3
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+print(f"o primeiro numero é : {numero1}")
+print(f"o segundo numero é : {numero2}")
