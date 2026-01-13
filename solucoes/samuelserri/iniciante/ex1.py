ab9add00216a67e3988f70920ac61e506107e6ea
Samuel Serri <samuelcerri@gmail.com>
2026-01-13 12:12:00 -0300

    solucao e criacao pasta

---
+++ solucoes/samuelserri/iniciante/ex1.py
@@ -0,0 +1,20 @@
+"""
+Exercício 01 — Variáveis
+
+Crie três variáveis:
+- nome (string)
+- idade (int)
+- cidade (string)
+
+Depois, imprima uma frase no seguinte formato:
+
+"Meu nome é João, tenho 25 anos e moro em São Paulo."
+"""
+
+# Escreva seu código abaixo
+
+
+nome = str(input("Nome:"))
+idade = int(input("Idade:"))
+cidade = str(input("Cidade:"))
+print("Meu nome é {nome}, tenho {idade} anos e moro em {cidade}.")
