41c9a55af0b11b86eb2e0cda51840669b028b709
xavier-sa <wellington222688@gmail.com>
2026-02-02 19:04:28 -0300

    feat: solucao exercicio 13

---
+++ solucoes/xavier/iniciante/ex13.py
@@ -0,0 +1,21 @@
+"""
+Exercício 13 — Função com retorno
+
+Crie uma função que receba dois números
+e retorne a soma deles.
+Depois, imprima o resultado.
+"""
+
+# Escreva seu código abaixo
+
+def funcaoRetorno():
+    numero1= int(input("Informe o numero: "))
+    numero2= int(input("Informe o numero: "))
+    
+    soma = numero1 + numero2
+    print(soma)
+
+
+
+
+funcaoRetorno()
